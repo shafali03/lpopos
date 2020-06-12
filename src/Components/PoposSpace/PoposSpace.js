2e336b84dfd3a7520aecb6b938b49093ed2234dd
shafali03 <shaf03sm@gmail.com>
2020-06-12 21:32:55 +0100

    feat: Displaying all image with name and address

--- src/Components/PoposSpace/PoposSpace.js
+++ src/Components/PoposSpace/PoposSpace.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function PoposSpace() {
+function PoposSpace(props) {
+  const { name, image, address } = props
+
   return (
     <div data-test='component-poposSpace'>
-      <img src="" width="300" height="300" alt="Hello" />
-      <h1>Name...</h1>
-      <div>Address</div>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+      <h1>{name}</h1>
+      <div>{address}</div>
     </div>
   )
 }
