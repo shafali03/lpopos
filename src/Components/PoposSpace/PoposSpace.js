4c1ee9f3c1ba1027fea0005a28096201656f6446
shafali03 <shaf03sm@gmail.com>
2020-06-13 09:51:04 +0100

    styles: Added styles to PoposList and PoposSpace

--- src/Components/PoposSpace/PoposSpace.js
+++ src/Components/PoposSpace/PoposSpace.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import './PoposSpace.css'
 
 function PoposSpace(props) {
   const { name, image, address } = props
 
   return (
     <div data-test='component-poposSpace'>
-      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-      <h1>{name}</h1>
-      <div>{address}</div>
+      <img className="PoposSpace" src={`${process.env.PUBLIC_URL}images/${image}`}
+        width="300" height="300" alt="Hello" />
+      <h3 className="PoposSpace">{name}</h3>
+      <div className="PoposSpace">{address}</div>
     </div>
   )
 }
